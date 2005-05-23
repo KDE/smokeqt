876f9deb0a1ddc052ae1634531c4bf2c2c470b29
Richard Dale <richard.j.dale@gmail.com>
2005-05-23 07:19:16 +0000

    * Promoted fixes for improved ruby Smoke lib introspection to the   release branch
    svn path=/branches/KDE/3.4/kdebindings/smoke/; revision=417243

--- smoke.h
+++ smoke.h
@@ -4,8 +4,35 @@
 #include <string.h>
 
 /*
- *   Copyright (C) 2002, Ashley Winters <qaqortog@nwlink.com>
- */
+   Copyright (C) 2002, Ashley Winters <qaqortog@nwlink.com>
+
+    BSD License
+
+    Redistribution and use in source and binary forms, with or
+      without modification, are permitted provided that the following
+      conditions are met:
+
+    Redistributions of source code must retain the above
+      copyright notice, this list of conditions and the following disclaimer.
+
+    Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials
+      provided with the distribution.>
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY
+    EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+    THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+    PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+    EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+    TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+    IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+    THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 class SmokeBinding;
 
@@ -50,7 +77,11 @@ public:
         mf_static = 0x01,
         mf_const = 0x02,
         mf_copyctor = 0x04,  // Copy constructor
-        mf_internal = 0x08   // For internal use only
+        mf_internal = 0x08,   // For internal use only
+        mf_enum = 0x10,   // An enum value
+        mf_ctor = 0x20,
+        mf_dtor = 0x40,
+        mf_protected = 0x80
     };
     /**
      * Describe one method of one class.
