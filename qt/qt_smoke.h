29ee9454ba507307f099962b6bc0591ff28ffd3c
Richard Dale <richard.j.dale@gmail.com>
2007-02-16 17:45:24 +0000

    * Use the Q_DECL_EXPORT macro instead of the KDE_EXPORT one to avoid kde   dependencies in the qt-only part of qtruby and the smoke library.
    CCMAIL: kde-bindings@kde.org
    
    
    svn path=/trunk/KDE/kdebindings/smoke/; revision=634246

--- qt/qt_smoke.h
+++ qt/qt_smoke.h
@@ -1,9 +1,11 @@
 #ifndef QT_SMOKE_H
 #define QT_SMOKE_H
-#include <kdemacros.h>
+
+// Don't use kdemacros.h/KDE_EXPORT here as it needs to be free of KDE dependencies
+#include <QtCore/qglobal.h>
 // Defined in smokedata.cpp, initialized by init_qt_Smoke(), used by all .cpp files
-extern KDE_EXPORT Smoke* qt_Smoke;
-extern KDE_EXPORT void init_qt_Smoke();
+extern Q_DECL_EXPORT Smoke* qt_Smoke;
+extern Q_DECL_EXPORT void init_qt_Smoke();
 
 class QGlobalSpace { };
 
