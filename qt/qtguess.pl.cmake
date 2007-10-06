60336137f463a6c2d442e1460ebee18ae9880e1c
Richard Dale <richard.j.dale@gmail.com>
2007-10-06 16:25:54 +0000

    * Remove commented out code
    svn path=/trunk/KDE/kdebindings/smoke/; revision=722094

--- qt/qtguess.pl.cmake
+++ qt/qtguess.pl.cmake
@@ -39,19 +39,6 @@ my $qtcoreinc = '@QT_QTCORE_INCLUDE_DIR@';
 my $allinc = '@all_includes@';
 my $alllib = '@all_libs@';
 
-# my %x;
-# $x{'LIBPNG'}  =   '@LIBPNG@';
-# $x{'LIBJPEG'} =   '@LIBJPEG@';
-# $x{'LIBSM'}   =   '@LIBSM@';
-# $x{'LIBSOCKET'} = '@LIBSOCKET@';
-# $x{'LIBDL'}      = '@LIBDL@';
-# $x{'LIBRESOLV'}  = '@LIBRESOLV@';
-# $x{'LIB_X11'} =   '@LIB_X11@';
-# $x{'X_PRE_LIBS'} = '@X_PRE_LIBS@';
-# $x{'LIB_X11'} =~ s/\$\((.*?)\)/$x{$1}/g;
-# 
-# $qtflags =~ s/\$\((.*?)\)/$x{$1}/g;
-
  -e "$qtcoreinc/qglobal.h" or die "Invalid Qt include directory.\n";
 
 my $ccmd = "$cc $ccflags $allinc $alllib -o $tmp $tmp.cpp";
@@ -68,7 +55,6 @@ map{ $tests{$_}->[2]>=$threshold ? ($used++, $total++):$total++ } keys %tests;
 print "Number of defines to be tested : $used/$total\n\n" unless $opt_q;
 open( QTDEFS, ">>".($opt_o || "qtdefines") ) or die "Can't open output file: $!\n";
 
-#grab_qglobal_symbols();
 preliminary_test();
 perform_all_tests();
 
@@ -90,37 +76,6 @@ sub gettmpfile
 	return "$tmpdir/$tmpname";
 }
 
-#--------------------------------------------------------------#
-
-sub grab_qglobal_symbols
-{
-	my $cmd = "$cc -E -D__cplusplus -dM -I$qtcoreinc $qtcoreinc/qglobal.h 2>/dev/null";
-	my $symbols = `$cmd`;
-        for(0..1)
-        {
-	    if( check_exit_status($?) )
-	    {
-		while( $symbols =~/^#\s*define\s*(QT_\S+)/gm )
-		{
-			$qtdefs{$1} = 1;
-		}
-		print "Found ". scalar( keys %qtdefs )." predefined symbol".((scalar( keys %qtdefs ) -1)?"s":"")." in qglobal.h\n" unless ($opt_q or !(keys %qtdefs));
-		while( $symbols =~/^#\s*define\s*QT_MODULE_(\S+)/gm )
-		{
-			$qtundefs{"QT_NO_$1"} = 1;
-		}
-		print "Found ". scalar( keys %qtundefs )." undefined symbol".((scalar( keys %qtundefs ) -1)?"s":"")." in qglobal.h\n" unless ($opt_q or !(keys %qtundefs));
-                last;
-	    }
-	    elsif(! $_) # first try
-	    {
-		print  "Failed to run $cmd.\nTrying without __cplusplus (might be already defined)\n";
-                $cmd = "$cc -E -dM -I$qtcoreinc $qtcoreinc/qglobal.h 2>/dev/null";
-                $symbols = `$cmd`;
-                next;
-	    }
-        }
-}
 
 #--------------------------------------------------------------#
 
@@ -250,7 +205,6 @@ BEGIN {
 our %tests = (
 	"QT_NO_ACCESSIBILITY" =>        ["QtGui/qaccessible.h", "QAccessibleEvent event(QEvent::AccessibilityHelp, 0);", 15],
 	"QT_NO_ACTION" =>		["QtGui/qaction.h", "QAction foo( (QObject*)NULL );", 5],
-	#QT_NO_BIG_CODECS
  	"QT_NO_BUTTONGROUP" =>		["QtGui/qbuttongroup.h", "QButtonGroup foo( (QObject*)NULL );", 12],
  	"QT_NO_CHECKBOX" =>		["QtGui/qcheckbox.h", "QCheckBox foo( (QWidget*)NULL );", 10],
 	"QT_NO_CLIPBOARD" => 		["QtGui/qapplication.h, QtGui/qclipboard.h", q"
@@ -258,7 +212,6 @@ our %tests = (
 						QClipboard *baz= foo.clipboard();
 					", 5],
  	"QT_NO_COLORDIALOG" =>		["QtGui/qcolordialog.h", "QColorDialog::customCount();", 12],
-	#QT_NO_COLORNAMES
  	"QT_NO_COMBOBOX" =>		["QtGui/qcombobox.h", "QComboBox foo( (QWidget*)NULL );", 10],
 	"QT_NO_COMPAT" =>		["QtGui/qfontmetrics.h", q"
 						QFontMetrics *foo= new QFontMetrics( QFont() );
@@ -268,38 +221,27 @@ our %tests = (
  						QApplication foo( argc, argv );
  						foo.addLibraryPath( QString::null );
 					", 5],
-	#QT_NO_COP
  	"QT_NO_CURSOR" =>		["QtGui/qcursor.h", "QCursor foo;", 5],
  	"QT_NO_DATASTREAM" =>		["QtCore/qdatastream.h", "QDataStream foo;", 5],
  	"QT_NO_DATETIMEEDIT" =>		["QtGui/qdatetimeedit.h", "QTimeEdit foo;", 12],
 	"QT_NO_DIAL" =>			["QtGui/qdial.h", "QDial foo;", 10],
 	"QT_NO_DIALOG" =>		["QtGui/qdialog.h", "QDialog foo;", 12],
 	"QT_NO_DIR" =>			["QtCore/qdir.h", "QDir foo;", 5],
-	#QT_NO_DIRECTPAINTER
 	"QT_NO_DOM" =>			["QtXml/qdom.h", "QDomDocumentType foo;", 5],
 	"QT_NO_DRAGANDDROP" =>		["QtGui/qevent.h", "QDropEvent foo( QDropEvent() );", 5],
 	"QT_NO_DRAWUTIL" =>		["QtGui/qdrawutil.h, QtGui/qcolor.h", "qDrawPlainRect( (QPainter *) NULL, 0, 0, 0, 0, QColor() );", 10],
-	#QT_NO_EFFECTS
 	"QT_NO_ERRORMESSAGE" => 	["QtGui/qerrormessage.h", "QErrorMessage foo( (QWidget*) NULL );", 13],
 	"QT_NO_FILEDIALOG" =>		["QtGui/qfiledialog.h", "QFileDialog foo();", 13],
 
 	"QT_NO_FONTDATABASE" =>		["QtGui/qfontdatabase.h", "QFontDatabase foo;", 5],
 	"QT_NO_FONTDIALOG" => 		["QtGui/qfontdialog.h",   "QFontDialog::getFont( (bool *)NULL );", 12],
 	"QT_NO_FRAME" => 		["QtGui/qframe.h", "QFrame foo;", 10],
-	#QT_NO_FREETYPE
  	"QT_NO_FTP" =>	["QtNetwork/qftp.h", "QFtp foo;", 9],
 	"QT_NO_GROUPBOX" =>		["QtGui/qgroupbox.h", "QGroupBox foo;", 12],
-	#QT_NO_IMAGEFORMAT_BMP
-	#QT_NO_IMAGEFORMAT_JPEG
-	#QT_NO_IMAGEFORMATPLUGIN
-	#QT_NO_IMAGEFORMAT_PNG
-	#QT_NO_IMAGEFORMAT_PPM
-	#QT_NO_IMAGEFORMAT_XBM
  	"QT_NO_IMAGE_HEURISTIC_MASK" =>	["QtGui/qimage.h", q"
 						QImage *foo = new QImage;
 						foo->createHeuristicMask();
 					", 8],
-	#QT_NO_IMAGE_TEXT
 	"QT_NO_IMAGEIO" => 		["QtGui/qbitmap.h, QtCore/qstring.h", q"
 						QBitmap foo( QString::fromLatin1('foobar') );
 					", 5],
@@ -328,7 +270,6 @@ our %tests = (
 	"QT_NO_MESSAGEBOX" =>		["QtGui/qmessagebox.h", "QMessageBox foo;", 13],
 	"QT_NO_MOVIE" =>		["QtGui/qmovie.h", "QMovie foo;", 5],
 	"QT_NO_NETWORK" =>		["QtNetwork/qnetworkproxy.h", "QNetworkProxy foo;", 5],
-	#QT_NO_NETWORKPROXY
 	"QT_NO_PALETTE" =>		["QtGui/qpalette.h", "QPalette foo;", 5],
 	"QT_NO_PICTURE" =>		["QtGui/qpicture.h", "QPicture foo;", 5],
 	"QT_NO_PRINTER" =>		["QtGui/qprinter.h", "QPrinter foo();", 5],
@@ -340,35 +281,16 @@ our %tests = (
 	"QT_NO_PUSHBUTTON" =>		["QtGui/qpushbutton.h", "QPushButton foo( (QWidget *) NULL );", 12],
 	"QT_NO_QUUID_STRING" =>		["QtCore/quuid.h", "QUuid foo( QString() );", 8],
 	"QT_NO_RADIOBUTTON" =>		["QtGui/qradiobutton.h", "QRadioButton foo( (QWidget *) NULL );", 12],
-	#QT_NO_QWS_ALPHA_CURSOR
-	#QT_NO_QWS_DECORATION_DEFAULT
-	#QT_NO_QWS_INPUTMETHODS
-	#QT_NO_QWS_KEYBOARD
-	#QT_NO_QWS_MOUSE
-	#QT_NO_QWS_MOUSE_AUTO
-	#QT_NO_QWS_MOUSE_MANUAL
-	#QT_NO_QWS_MULTIPROCESS
-	#QT_NO_QWS_PROPERTIES
-	#QT_NO_QWS_QPF
-	#QT_NO_RESIZEHANDLER
-	#QT_NO_RUBBERBAND
 	"QT_NO_SCROLLBAR" =>		["QtGui/qscrollbar.h", "QScrollBar foo( (QWidget *) NULL );", 12],
 	"QT_NO_SESSIONMANAGER" =>	["QtGui/qapplication.h", q"
   						QApplication foo( argc, argv );
   						foo.sessionId();
 					", 15],
 	"QT_NO_SETTINGS" =>		["QtCore/qsettings.h", "QSettings foo;", 5],
-	#QT_NO_SHORTCUT
 	"QT_NO_SIGNALMAPPER" =>		["QtCore/qsignalmapper.h", "QSignalMapper foo( (QObject *) NULL );", 0],
 	"QT_NO_SIZEGRIP" =>		["QtGui/qsizegrip.h", "QSizeGrip foo( (QWidget *) NULL );", 10],
 	"QT_NO_SLIDER" =>		["QtGui/qslider.h", "QSlider foo( (QWidget *) NULL );", 12],
 	"QT_NO_SOUND" =>		["QtGui/qsound.h", "QSound foo( QString::null );", 5],
-	#QT_NO_SPINWIDGET
-	#QT_NO_SPLASHSCREEN
-	#QT_NO_STACKEDWIDGET
-	#QT_NO_STATUSBAR
-	#QT_NO_STATUSTIP
-	#QT_NO_STL
 	"QT_NO_STRINGLIST" =>		["qstringlist.h", "QStringList foo;", 0],
 	"QT_NO_STYLE_PLASTIQUE" =>		["QtGui/qplastiquestyle.h", "QPlastiqueStyle foo;", 16],
  	"QT_NO_STYLE_WINDOWSXP" =>	["QtGui/qwindowsxpstyle.h", "QWindowsXPStyle foo;", 16],
@@ -394,7 +316,6 @@ our %tests = (
 						};
 						Q_EXPORT_PLUGIN2( 'Foo', QFoo )
 					"],
-	#QT_NO_TEXTDATE
  	"QT_NO_TEXTEDIT" =>		["QtGui/qtextedit.h", "QTextEdit foo;", 13], 
     "QT_NO_TEXTSTREAM" =>           ["QtCore/qtextstream.h", "QTextStream foo;", 5],
     "QT_NO_THREAD" =>           ["QtCore/qthread.h", "QFoo foo;", 5, q"
@@ -409,10 +330,6 @@ our %tests = (
     "QT_NO_TOOLBAR" =>              ["QtGui/qtoolbar.h", "QToolBar foo;", 10],
     "QT_NO_TOOLBUTTON" =>           ["QtGui/qtoolbutton.h", "QToolButton foo((QWidget *) NULL );", 12],
     "QT_NO_TOOLTIP" =>              ["QtGui/qtooltip.h", "QToolTip::palette();", 10],
-	#QT_NO_TRANSLATION
-	#QT_NO_UDPSOCKET
-	#QT_NO_URLINFO
-	#QT_NO_VALIDATOR
     "QT_NO_VARIANT" =>              ["QtCore/qvariant.h", "QVariant foo;", 0],
     "QT_NO_WHATSTHIS" =>            ["QtGui/qwhatsthis.h", "QWhatsThis::inWhatsThisMode();", 10],
 	"QT_NO_WHEELEVENT" =>		["QtGui/qevent.h", "QWheelEvent foo( QPoint(1,1), 1, (Qt::MouseButtons)1, 0 );", 5],
