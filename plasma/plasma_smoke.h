69bbd515dd2f38384f910485ca368a273f9456e8
Richard Dale <richard.j.dale@gmail.com>
2008-05-19 09:29:11 +0000

    * Add smoke libs for Plasma and Soprano
    CCMAIL: kde-bindings@kde.org
    
    svn path=/trunk/KDE/kdebindings/smoke/; revision=809699

---
+++ plasma/plasma_smoke.h
@@ -0,0 +1,14 @@
+#ifndef PLASMA_SMOKE_H
+#define PLASMA_SMOKE_H
+
+#include <smoke.h>
+
+extern SMOKE_EXPORT Smoke* plasma_Smoke;
+extern SMOKE_EXPORT void init_plasma_Smoke();
+
+#ifndef QGLOBALSPACE_CLASS
+#define QGLOBALSPACE_CLASS
+class QGlobalSpace { };
+#endif
+
+#endif
---
+++ soprano/soprano_smoke.h
@@ -0,0 +1,15 @@
+#ifndef SOPRANO_SMOKE_H
+#define SOPRANO_SMOKE_H
+
+#include <smoke.h>
+
+// Defined in smokedata.cpp, initialized by init_soprano_Smoke(), used by all .cpp files
+extern SMOKE_EXPORT Smoke* soprano_Smoke;
+extern SMOKE_EXPORT void init_soprano_Smoke();
+
+#ifndef QGLOBALSPACE_CLASS
+#define QGLOBALSPACE_CLASS
+class QGlobalSpace { };
+#endif
+
+#endif
