60d14e26eaf2f31032d6b2f937a82572714eda5f
Richard Dale <richard.j.dale@gmail.com>
2008-05-31 15:12:57 +0000

    * Add an Okular smoke lib and split off the QtUiTools module into its own smoke lib as   it is linked to a large static library
    CCMAIL: kde-bindings@kde.org
    
    svn path=/trunk/KDE/kdebindings/smoke/; revision=814878

---
+++ qtuitools/qtuitools_smoke.h
@@ -0,0 +1,15 @@
+#ifndef QTUITOOLS_SMOKE_H
+#define QTUITOOLS_SMOKE_H
+
+#include <smoke.h>
+
+// Defined in smokedata.cpp, initialized by init_qsci_Smoke(), used by all .cpp files
+extern SMOKE_EXPORT Smoke* qtuitools_Smoke;
+extern SMOKE_EXPORT void init_qtuitools_Smoke();
+
+#ifndef QGLOBALSPACE_CLASS
+#define QGLOBALSPACE_CLASS
+class QGlobalSpace { };
+#endif
+
+#endif
