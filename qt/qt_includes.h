7a5b91f79450a6111dbd55b4509c890c342077aa
Richard Dale <richard.j.dale@gmail.com>
2009-12-01 19:59:36 +0000

    * Add the QX11Info, QX11EmbedContainer and QX11EmbedWidget classes to the monolithic qt smoke lib
    CCMAIL: kde-bindings@kde.org
    
    svn path=/trunk/KDE/kdebindings/smoke/; revision=1057244

--- qt/qt_includes.h
+++ qt/qt_includes.h
@@ -1,5 +1,11 @@
 #include <QtCore/QtCore>
 #include <QtGui/QtGui>
+
+#ifdef Q_WS_X11
+#include <QtGui/qx11info_x11.h>
+#include <QtGui/qx11embed_x11.h>
+#endif
+
 #include <QtXml/QtXml>
 #include <QtSql/QtSql>
 #include <QtOpenGL/QtOpenGL>
