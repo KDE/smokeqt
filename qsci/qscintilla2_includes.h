174a39e9db450c5bc9bb8a3df3bd1373d9dc0a9e
Arno Rehn <arno@arnorehn.de>
2010-01-10 14:17:23 +0000

    bring the qscintilla module up-to-date with qscintilla 2.4
    svn path=/trunk/KDE/kdebindings/smoke/; revision=1072614

--- qsci/qscintilla2_includes.h
+++ qsci/qscintilla2_includes.h
@@ -1,3 +1,4 @@
+#include <qsciabstractapis.h>
 #include <qsciapis.h>
 #include <qscicommand.h>
 #include <qscicommandset.h>
@@ -9,8 +10,11 @@
 #include <qscilexercpp.h>
 #include <qscilexercsharp.h>
 #include <qscilexercss.h>
+#include <qscilexercustom.h>
 #include <qscilexerd.h>
 #include <qscilexerdiff.h>
+#include <qscilexerfortran77.h>
+#include <qscilexerfortran.h>
 #include <qscilexer.h>
 #include <qscilexerhtml.h>
 #include <qscilexeridl.h>
@@ -18,17 +22,24 @@
 #include <qscilexerjavascript.h>
 #include <qscilexerlua.h>
 #include <qscilexermakefile.h>
+#include <qscilexerpascal.h>
 #include <qscilexerperl.h>
+#include <qscilexerpostscript.h>
 #include <qscilexerpov.h>
 #include <qscilexerproperties.h>
 #include <qscilexerpython.h>
 #include <qscilexerruby.h>
 #include <qscilexersql.h>
+#include <qscilexertcl.h>
 #include <qscilexertex.h>
 #include <qscilexervhdl.h>
+#include <qscilexerxml.h>
+#include <qscilexeryaml.h>
 #include <qscimacro.h>
 #include <qsciprinter.h>
 #include <qsciscintillabase.h>
 #include <qsciscintilla.h>
+#include <qscistyledtext.h>
+#include <qscistyle.h>
 
 #include <qvariant.h>
