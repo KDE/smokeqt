9c1af5d786f7cda0381c7d650f720681ca5451e8
Ian Monroe <ian.monroe@gmail.com>
2011-01-11 16:53:18 -0500

    Removed smokekde.
    You may need to use git log --follow to see commits after this point.

--- kde/knewstuff2/knewstuff2_includes.h
+++
@@ -1,8 +0,0 @@
-#include <knewstuff2/core/author.h>
-#include <knewstuff2/core/entry.h>
-#include <knewstuff2/core/category.h>
-#include <knewstuff2/core/ktranslatable.h>
-#include <knewstuff2/core/installation.h>
-#include <knewstuff2/ui/knewstuffaction.h>
-#include <knewstuff2/engine.h>
-#include <knewstuff2/knewstuff_export.h>
--- kde/knewstuff3/knewstuff3_includes.h
+++
@@ -1,9 +0,0 @@
-#include <kdeversion.h>
-#if KDE_IS_VERSION(4, 4, 0)
-#include <knewstuff3/downloaddialog.h>
-#include <knewstuff3/entry.h>
-#include <knewstuff3/knewstuff_export.h>
-#include <knewstuff3/knewstuffaction.h>
-#include <knewstuff3/knewstuffbutton.h>
-#include <knewstuff3/uploaddialog.h>
-#endif
