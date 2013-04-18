746e6037d085c5f1e00855247fd07ee2e1574e9d
Dimitar Dobrev <dpldobrev@yahoo.com>
2013-04-18 17:31:39 +0300

    Added QtQuick from Qt 5. There are a few errors in the generated code, such as usage of QSGTexturePrivate and some non-existent QQuickItem::realValue and similar.
    Signed-off-by: Dimitar Dobrev <dpldobrev@yahoo.com>

---
+++ qtquick_smoke.h
@@ -0,0 +1,16 @@
+#ifndef qtquick_SMOKE_H
+#define qtquick_SMOKE_H
+
+#include <smoke.h>
+
+// Defined in smokedata.cpp, initialized by init_qtquick_Smoke(), used by all .cpp files
+extern "C" SMOKE_EXPORT Smoke* qtquick_Smoke;
+extern "C" SMOKE_EXPORT void init_qtquick_Smoke();
+extern "C" SMOKE_EXPORT void delete_qtquick_Smoke();
+
+#ifndef QGLOBALSPACE_CLASS
+#define QGLOBALSPACE_CLASS
+class QGlobalSpace { };
+#endif
+
+#endif
