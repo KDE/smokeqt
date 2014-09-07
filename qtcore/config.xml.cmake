b6dbffe5a8bc1f0b1dd64079e71226ce74fc3128
Richard Dale <richard.dale@codethink.co.uk>
2014-09-07 13:30:15 +0100

    Fix build of Qt5 versions of QtCore and QtWidgets Smoke modules

--- qtcore/config.xml.cmake
+++ qtcore/config.xml.cmake
@@ -4,7 +4,7 @@
     <generator>smoke</generator>
     <definesList>@QTDEFINES_FILE@</definesList>
     <includeDirs>
-        <dir>@QT_HEADERS_DIR@</dir>
-        <dir>@QT_QTCORE_INCLUDE_DIR@</dir>
+        <dir>@Qt5_INCLUDE_DIR@</dir>
+        <dir>@Qt5Core_INCLUDE_DIR@</dir>
     </includeDirs>
 </config>
