17d915c22837a77af519d9225e7ac2eb74b1c6b7
Chris Burel <chrisburel@gmail.com>
2015-10-23 16:31:57 -0700

    Adjust qtcore build for qt5

--- qtcore/config.xml.cmake
+++ qtcore/config.xml.cmake
@@ -4,7 +4,6 @@
     <generator>smoke</generator>
     <definesList>@QTDEFINES_FILE@</definesList>
     <includeDirs>
-        <dir>@QT_HEADERS_DIR@</dir>
-        <dir>@QT_QTCORE_INCLUDE_DIR@</dir>
+@qt_dirs@
     </includeDirs>
 </config>
