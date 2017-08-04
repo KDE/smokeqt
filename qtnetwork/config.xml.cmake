639424030a81be3ce7de09615815e6f5b175cf21
Chris Burel <chrisburel@gmail.com>
2017-08-04 08:47:13 -0700

    Adjust qtnetwork build for qt5

--- qtnetwork/config.xml.cmake
+++ qtnetwork/config.xml.cmake
@@ -4,8 +4,6 @@
     <generator>smoke</generator>
     <definesList>@QTDEFINES_FILE@</definesList>
     <includeDirs>
-        <dir>@QT_HEADERS_DIR@</dir>
-        <dir>@QT_QTCORE_INCLUDE_DIR@</dir>
-        <dir>@QT_QTNETWORK_INCLUDE_DIR@</dir>
+@qt_dirs@
     </includeDirs>
 </config>
