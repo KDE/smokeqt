286da49cb6f8f787120ac107a646dee48de14866
Chris Burel <chrisburel@gmail.com>
2017-08-04 18:58:39 -0700

    Add QtQml module.

---
+++ qtqml_smoke.h
@@ -0,0 +1,16 @@
+#ifndef QTQML_SMOKE_H
+#define QTQML_SMOKE_H
+
+#include <smoke.h>
+
+// Defined in smokedata.cpp, initialized by init_qtsmoke_Smoke(), used by all .cpp files
+extern "C" SMOKE_EXPORT Smoke* qtqml_Smoke;
+extern "C" SMOKE_EXPORT void init_qtqml_Smoke();
+extern "C" SMOKE_EXPORT void delete_qtqml_Smoke();
+
+#ifndef QGLOBALSPACE_CLASS
+#define QGLOBALSPACE_CLASS
+class QGlobalSpace { };
+#endif
+
+#endif
